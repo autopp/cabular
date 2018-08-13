acb6005076b251e54bf69fbe2a3b24dd9ff2b426
autopp <autopp.inc@gmail.com>
2018-08-13 21:48:38 +0900

    test: check exit status of each example

--- example/run_test.sh
+++ example/run_test.sh
@@ -2,7 +2,22 @@
 
 set -eu
 
+errors=0
 for exefile in $* ; do
-  echo run $exefile
-  ./$exefile
+  echo -n "$exefile: "
+  status=0
+  stdout=$(./$exefile) || status=$?
+  if [[ "$status" -eq 0 ]]; then
+    echo "OK"
+  else
+    errors=$((errors+1))
+    echo "NG"
+  fi
 done
+
+if [[ "$errors" -eq 0 ]]; then
+  echo all examples finished
+else
+  echo "$errors example(s) are failed"
+  exit 1
+fi
