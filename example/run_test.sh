d25208e55f5ceb77c6459a55b99649850ace7076
autopp <autopp.inc@gmail.com>
2018-08-13 22:06:13 +0900

    test: check stdout of examples

--- example/run_test.sh
+++ example/run_test.sh
@@ -5,13 +5,19 @@ set -eu
 errors=0
 for exefile in $* ; do
   echo -n "$exefile: "
+  expected_stdout_file=${exefile/.out/.stdout}
   status=0
   stdout=$(./$exefile) || status=$?
-  if [[ "$status" -eq 0 ]]; then
-    echo "OK"
-  else
+  if [[ "$status" -ne 0 ]]; then
     errors=$((errors+1))
     echo "NG"
+    echo "exit status is not zero"
+  elif stdout_diff=$(diff $expected_stdout_file <(echo "$stdout")) ; then
+    echo "OK"
+  else
+    echo "NG"
+    echo "stdout is not expected contents"
+    echo "$stdout_diff"
   fi
 done
 
