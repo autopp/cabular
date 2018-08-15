7cde6e9c12ae57b3230745ae5480f7ebe418ed9b
autopp <autopp.inc@gmail.com>
2018-08-15 09:49:51 +0900

    test: add example

---
+++ example/multi.c
@@ -0,0 +1,27 @@
+#include <cabular.h>
+
+int store(int *addr, int new) {
+  int old = *addr;
+  *addr = new;
+  return old;
+}
+
+cabular {
+  suite(store) {
+    where(int var; int old; int new;) {
+      {0, 0, 1},
+      {0, 1, 2}
+    };
+
+    with_them {
+      c->var = c->old;
+      expect_that(store(&c->var, c->new) == c->old);
+    }
+
+    with_them {
+      c->var = c->old;
+      store(&c->var, c->new);
+      expect_that(c->var == c->new);
+    }
+  }
+}
