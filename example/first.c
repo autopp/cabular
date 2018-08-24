bf103dbdec82c75335271182599a1b7b88d47d8b
autopp <autopp.inc@gmail.com>
2018-08-24 22:59:44 +0900

    test: replace new syntax

--- example/first.c
+++ example/first.c
@@ -13,23 +13,23 @@ double mymax(double x, double y) {
 
 cabular {
   suite(mystrlen) {
-    where(const char *input; size_t expected;) {
+    patterns(inout, const char *input; size_t expected;) {
       {"hello", 5},
       {"", 0}
     };
 
-    with_them {
+    test_with(return_value, inout, c) {
       expect_that(mystrlen(c->input) == c->expected);
     }
   }
 
   suite(mymax) {
-    where(double x; double y; double expected;) {
+    patterns(inout, double x; double y; double expected;) {
       {.x = 1.1, .y = 1.0, .expected = 1.1},
       {.x = 0.9, .y = 1.0, .expected = 1.0},
     };
 
-    with_them {
+    test_with(return_value, inout, c) {
       expect_that(mymax(c->x, c->y) == c->expected);
     }
   }
--- example/multi.c
+++ example/multi.c
@@ -8,22 +8,6 @@ int store(int *addr, int new) {
 
 cabular {
   suite(store) {
-    where(int var; int old; int new;) {
-      {0, 0, 1},
-      {0, 1, 2}
-    };
-
-    with_them {
-      c->var = c->old;
-      expect_that(store(&c->var, c->new) == c->old);
-    }
-
-    with_them {
-      c->var = c->old;
-      store(&c->var, c->new);
-      expect_that(c->var == c->new);
-    }
-
     test(return_value) {
       int x = 42;
       expect_that(store(&x, 1) == 42);
