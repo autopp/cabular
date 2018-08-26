f3bb3bc7a5004b29eb2065d850c691045a9b3dfd
autopp <autopp.inc@gmail.com>
2018-08-26 22:41:09 +0900

    feat: add newline for readability of output

--- cabular.h
+++ cabular.h
@@ -26,7 +26,8 @@
   for (size_t cabular_case_counter, cabular_suite_executed = 0; !cabular_suite_executed || (printf("\n"), 0); cabular_suite_executed = 1)
 
 #define test(name)\
-  printf("  %s: ", cabular_make_str(name));
+  printf("  %s: ", cabular_make_str(name));\
+  for (size_t cabular_test_executed = 0; !cabular_test_executed || (printf("\n"), 0); cabular_test_executed = 1)
 
 #define patterns(name, ...)\
   struct cabular_pattern_type(name) { __VA_ARGS__ } cabular_patterns_var(name)[] =
@@ -34,7 +35,7 @@
 #define test_with(name, patterns, pattern)\
   printf("  %s: ", cabular_make_str(name));\
   cabular_case_counter = 0;\
-  for (struct cabular_pattern_type(patterns) *pattern = cabular_patterns_var(patterns); cabular_case_counter < sizeof(cabular_patterns_var(patterns)) / sizeof(cabular_patterns_var(patterns)[0]); cabular_case_counter++, pattern++)
+  for (struct cabular_pattern_type(patterns) *pattern = cabular_patterns_var(patterns); cabular_case_counter < sizeof(cabular_patterns_var(patterns)) / sizeof(cabular_patterns_var(patterns)[0]) || (printf("\n"), 0); cabular_case_counter++, pattern++)
 
 #define expect_that(expr)\
   printf("%s", expr ? "." : ((*cabular_failed_count += 1), "F"));
