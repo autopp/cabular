edfe1915b96888819c0a72612c3111a4504652ef
autopp <autopp.inc@gmail.com>
2018-09-01 21:05:39 +0900

    refactor: define struct for failure infomation

--- cabular.h
+++ cabular.h
@@ -10,13 +10,17 @@
 
 #define cabular_patterns_var(name) cabular_patterns_##name
 
-#define MAX_FAILURE_MSGS 100
+#define MAX_FAILURES 100
 
 /* test context */
+struct cabular_failure_t {
+  const char *msg;
+};
+
 struct cabular_ctx_t {
   int failed_count;
   int is_failed;
-  const char *failure_msgs[MAX_FAILURE_MSGS];
+  struct cabular_failure_t failures[MAX_FAILURES];
 };
 
 /* public APIs */
@@ -27,7 +31,7 @@ struct cabular_ctx_t {
     cabular_main(&cabular_ctx);\
     printf("%d failure(s)\n", cabular_ctx.failed_count);\
     for (int i = 0; i < cabular_ctx.failed_count; i++) {\
-      printf("  %s\n", cabular_ctx.failure_msgs[i]);\
+      printf("  %s\n", cabular_ctx.failures[i].msg);\
     }\
     return !!cabular_ctx.failed_count;\
   }\
@@ -48,8 +52,13 @@ struct cabular_ctx_t {
   cabular_case_counter = 0;\
   for (struct cabular_pattern_type(patterns) *pattern = cabular_patterns_var(patterns); ((cabular_ctx->is_failed = 0), cabular_case_counter) < sizeof(cabular_patterns_var(patterns)) / sizeof(cabular_patterns_var(patterns)[0]) || (printf("\n"), 0); printf("%s", cabular_ctx->is_failed ? ((cabular_ctx->failed_count += 1), "F") : "."), cabular_case_counter++, pattern++)
 
-#define failure(msg) (cabular_ctx->failure_msgs[cabular_ctx->failed_count] = msg, cabular_ctx->is_failed = 1)
+void failure_impl(struct cabular_ctx_t *ctx, const char *msg) {
+  ctx->failures[ctx->failed_count].msg = msg;
+  ctx->is_failed = 1;
+}
+#define failure(msg) (failure_impl(cabular_ctx, msg))
 
-#define expect_that(expr) ((expr) || failure("assertion is failed: " cabular_make_str(expr)))
+#define expect_that(expr) if (!(expr)) failure("assertion is failed: " cabular_make_str(expr))
+
 
 #endif
