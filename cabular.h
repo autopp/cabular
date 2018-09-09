5d12504165d8459dc8df7f1d7197a75400d141d3
autopp <autopp.inc@gmail.com>
2018-09-09 23:17:57 +0900

    fix: use linked list for failures store instead of fixed array

--- cabular.h
+++ cabular.h
@@ -2,6 +2,7 @@
 #define CABULAR_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /* macro utilities */
 #define cabular_make_str(token) #token
@@ -10,32 +11,31 @@
 
 #define cabular_patterns_var(name) cabular_patterns_##name
 
-#define MAX_FAILURES 100
-
 /* test context */
 struct cabular_failure_t {
   const char *filename;
   int line;
   const char *pattern_str;
   const char *msg;
+  struct cabular_failure_t *next;
 };
 
 struct cabular_ctx_t {
   int failed_count;
   int is_failed;
   const char *current_pattern_str;
-  struct cabular_failure_t failures[MAX_FAILURES];
+  struct cabular_failure_t *first_failure, *last_failure;
 };
 
 /* public APIs */
 #define cabular\
   void cabular_main(struct cabular_ctx_t *cabular_ctx);\
   int main(void) {\
-    struct cabular_ctx_t cabular_ctx = { .failed_count = 0 };\
+    struct cabular_ctx_t cabular_ctx = { .failed_count = 0, .first_failure = NULL, .last_failure = NULL };\
     cabular_main(&cabular_ctx);\
     printf("%d failure(s)\n", cabular_ctx.failed_count);\
-    for (int i = 0; i < cabular_ctx.failed_count; i++) {\
-      printf("  %s:%d\n    pattern: %s\n    %s\n", cabular_ctx.failures[i].filename, cabular_ctx.failures[i].line, cabular_ctx.failures[i].pattern_str, cabular_ctx.failures[i].msg);\
+    for (struct cabular_failure_t *f = cabular_ctx.first_failure; f != NULL; f = f->next) {\
+      printf("  %s:%d\n    pattern: %s\n    %s\n", f->filename, f->line, f->pattern_str, f->msg);\
     }\
     return !!cabular_ctx.failed_count;\
   }\
@@ -59,10 +59,25 @@ struct cabular_ctx_t {
   for (struct cabular_pattern_type(patterns) *pattern = cabular_patterns_var(patterns); ((cabular_ctx->is_failed = 0), (cabular_ctx->current_pattern_str = pattern->cabular_pattern_str), cabular_case_counter) < sizeof(cabular_patterns_var(patterns)) / sizeof(cabular_patterns_var(patterns)[0]) || (printf("\n"), 0); printf("%s", cabular_ctx->is_failed ? ((cabular_ctx->failed_count += 1), "F") : "."), cabular_case_counter++, pattern++)
 
 void fail_impl(struct cabular_ctx_t *ctx, const char *filename, int line, const char *msg) {
-  ctx->failures[ctx->failed_count].filename = filename;
-  ctx->failures[ctx->failed_count].line = line;
-  ctx->failures[ctx->failed_count].pattern_str = ctx->current_pattern_str;
-  ctx->failures[ctx->failed_count].msg = msg;
+  struct cabular_failure_t *failure = malloc(sizeof(struct cabular_failure_t));
+
+  if (failure == NULL) {
+    fprintf(stderr, "%s:%d: cannot allocate memory for failure infomation\n", filename, line);
+    abort();
+  }
+  failure->filename = filename;
+  failure->line = line;
+  failure->pattern_str = ctx->current_pattern_str;
+  failure->msg = msg;
+  failure->next = NULL;
+
+  if (ctx->first_failure == NULL) {
+    ctx->first_failure = ctx->last_failure = failure;
+  } else {
+    ctx->last_failure->next = failure;
+    ctx->last_failure = failure;
+  }
+
   ctx->is_failed = 1;
 }
 #define fail(msg)\
