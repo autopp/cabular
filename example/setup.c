cffcd94555db034d6d52ca120ba9f50abc8fbc48
autopp <autopp.inc@gmail.com>
2018-09-13 09:35:07 +0900

    test: add example of test setup

---
+++ example/setup.c
@@ -0,0 +1,30 @@
+#include <cabular.h>
+
+int store(int *addr, int new) {
+  int old = *addr;
+  *addr = new;
+  return old;
+}
+
+cabular {
+  suite(store) {
+    int var;
+    setup {
+      var = 42;
+    }
+
+    test(return_value) {
+      expect_that(store(&x, 1) == 42);
+    }
+
+    patterns(store_inout, int new;) {
+      pattern(2),
+      pattern(3)
+    };
+
+    test_with(stored_value, store_inout, c) {
+      store(&var, c->new);
+      expect_that(var == c->new);
+    }
+  }
+}
